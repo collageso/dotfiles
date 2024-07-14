334ab630a908e9d32318547e8e45eaae98d4864b
collageso <dnjsqhwo@gmail.com>
2024-07-14 16:57:19 +0900

    update config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,20 @@
 return {
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-            require('kanagawa').setup({
-        transparent = true, -- 투명도 설정 활성화
-        colors = {
-          theme = {
-            all = {
-              ui = {
-                bg_gutter = 'none' -- Gutter 배경도 투명하게 설정
-              }
-            }
-          }
-        }
-      })
-      vim.cmd.colorscheme "kanagawa-dragon"
-    end
-  }
+  'rebelot/kanagawa.nvim',
+  priority = 1000,
+  lazy = false,
+  config = function()
+    vim.cmd 'colorscheme kanagawa-dragon'
+  end,
+  opts = {
+    transparent = true,
+    colors = {
+      theme = {
+        all = {
+          ui = {
+            bg_gutter = 'none',
+          },
+        },
+      },
+    },
+  },
 }
