4925af7c95a5adab203bffc0d1a13bda50f633e3
collageso <dnjsqhwo@gmail.com>
2024-07-15 22:15:05 +0900

    update

--- .config/nvim/lua/base/options.lua
+++ .config/nvim/lua/base/options.lua
@@ -1,34 +1,53 @@
+-- Leader keys
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Encoding settings
 vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 
+-- UI settings
 vim.opt.number = true
 vim.opt.termguicolors = true
 
+-- Indentation settings
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 
+-- Wrapping and mouse settings
 vim.opt.wrap = false
 vim.opt.mouse = 'a'
+
+-- Display settings
 vim.opt.showmode = false
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.breakindent = true
-vim.opt.undofile = true
+vim.opt.signcolumn = 'yes'
+vim.opt.scrolloff = 10
+
+-- Search settings
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
+vim.opt.hlsearch = true
+
+-- File and buffer settings
+vim.opt.undofile = true
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
+
+-- Split settings
 vim.opt.splitright = true
 vim.opt.splitbelow = true
+
+-- Command settings
 vim.opt.inccommand = 'split'
-vim.opt.scrolloff = 10
-vim.opt.hlsearch = true
+
+-- Backspace behaviro
 vim.opt.backspace = { 'start', 'eol', 'indent' }
+
+-- Path and ignore settings
 vim.opt.path:append { '**' }
 vim.opt.wildignore:append { '*/node_modules/*' }
