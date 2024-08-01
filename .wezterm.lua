63c6cea0350fe2d4b52100a072baaeb1bf71c2af
collageso <dnjsqhwo@gmail.com>
2024-08-01 22:41:02 +0900

    added picom and background opacity

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm") 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono") 
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 12
 
 config.use_ime = true
