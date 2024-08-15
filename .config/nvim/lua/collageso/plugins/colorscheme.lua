15ee07a2f1b085402152001a931b848d908b2fb0
collageso <dnjsqhwo@gmail.com>
2024-08-15 17:27:38 +0900

    update config

--- .config/nvim/lua/collageso/plugins/colorscheme.lua
+++ .config/nvim/lua/collageso/plugins/colorscheme.lua
@@ -1,16 +1,16 @@
 return {
-  'rebelot/kanagawa.nvim',
-  priority = 1000,
-  lazy = false,
-  config = function()
-    require('kanagawa').setup({
-      theme = "dragon",
-      background = {
-        dark = "dragon",
-        light = "dragon"
-      },
-      transparent = true,
-    })
-    vim.cmd("colorscheme kanagawa")
-  end,
+	"rebelot/kanagawa.nvim",
+	priority = 1000,
+	lazy = false,
+	config = function()
+		require("kanagawa").setup({
+			theme = "dragon",
+			background = {
+				dark = "dragon",
+				light = "dragon",
+			},
+			transparent = true,
+		})
+		vim.cmd("colorscheme kanagawa")
+	end,
 }
