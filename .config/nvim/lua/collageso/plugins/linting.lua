23aaea6a202270b5ecb3f27bebd0a68910928075
collageso <dnjsqhwo@gmail.com>
2024-08-25 18:38:37 +0900

    update

--- .config/nvim/lua/collageso/plugins/linting.lua
+++ .config/nvim/lua/collageso/plugins/linting.lua
@@ -15,6 +15,14 @@ return {
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
+		lint.linters.eslint_d = require("lint.util").wrap(lint.linters.eslint_d, function(diagnostic)
+			-- try to ignore "No ESLint configuration found" error
+			if diagnostic.message:find("Error: Could not find config file") then
+				return nil
+			end
+			return diagnostic
+		end)
+
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
 			callback = function()
