57b3237eca7dd3a87f4752267dc848a73b7f7e13
collageso <dnjsqhwo@gmail.com>
2024-09-30 14:53:37 +0900

    update

--- .config/nvim/lua/collageso/plugins/treesitter.lua
+++ .config/nvim/lua/collageso/plugins/treesitter.lua
@@ -2,29 +2,9 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
-	dependencies = {
-		"windwp/nvim-ts-autotag",
-	},
 	config = function()
 		-- import nvim-treesitter plugin
 		local treesitter = require("nvim-treesitter.configs")
-
-		require("nvim-ts-autotag").setup({
-			opts = {
-				-- Defaults
-				enable_close = true, -- Auto close tags
-				enable_rename = true, -- Auto rename pairs of tags
-				enable_close_on_slash = false, -- Auto close on trailing </
-			},
-			-- Also override individual filetype configs, these take priority.
-			-- Empty by default, useful if one of the "opts" global settings
-			-- doesn't work well in a specific filetype
-			per_filetype = {
-				-- ["html"] = {
-				--   enable_close = false
-				-- }
-			},
-		})
 
 		-- configure treesitter
 		treesitter.setup({ -- enable syntax highlighting
