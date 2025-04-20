e70b2b5d42e297e55dee33f2cb83b3d878a7d1c7
collageso <dnjsqhwo@gmail.com>
2025-04-21 01:02:35 +0900

    update

--- .config/nvim/lua/collageso/plugins/telescope.lua
+++ .config/nvim/lua/collageso/plugins/telescope.lua
@@ -25,6 +25,9 @@ return {
 		telescope.setup({
 			defaults = {
 				path_display = { "smart" },
+				file_ignore_patterns = {
+					"node_modules",
+				},
 				mappings = {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
