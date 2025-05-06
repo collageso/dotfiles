562fe2bb8775cfc0f25b3e1aefc021e4a6e19bc5
collageso <dnjsqhwo@gmail.com>
2025-05-06 14:40:44 +0900

    update

--- .config/nvim/lua/collageso/core/options.lua
+++ .config/nvim/lua/collageso/core/options.lua
@@ -28,6 +28,22 @@ vim.api.nvim_create_autocmd("BufEnter", {
 	desc = "Disable New Line Comment",
 })
 
+-- Silence the specific position encoding message
+local notify_original = vim.notify
+vim.notify = function(msg, ...)
+	if
+		msg
+		and (
+			msg:match("position_encoding param is required")
+			or msg:match("Defaulting to position encoding of the first client")
+			or msg:match("multiple different client offset_encodings")
+		)
+	then
+		return
+	end
+	return notify_original(msg, ...)
+end
+
 opt.wrap = false
 
 -- search settings
